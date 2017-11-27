4e5498b339fe6330f815972fc5a99cefecae927d
Chandra Kant Sharma <chandu.rikku@gmail.com>
2017-11-28 03:39:43 +0530

    Create factorial_trailing_zeros.py

---
+++ Math/factorial_trailing_zeros.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def trailingZeroes(self, A):
+        count = 0
+        
+        while A > 4:
+            
+            count += A//5 
+            A = A//5
+        return count
