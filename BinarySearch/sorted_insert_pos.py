eac66f2aa3fa59cf6ccc8fc5e996dada8adbfcb2
Chandra Kant Sharma <chandu.rikku@gmail.com>
2017-12-02 05:47:19 +0530

    Create sorted_insert_pos.py

---
+++ BinarySearch/sorted_insert_pos.py
@@ -0,0 +1,24 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def searchInsert(self, A, B):
+        
+        n = len(A)
+        
+        low = 0
+        high = n - 1
+        result1 = -1
+        
+        while low <= high:
+            mid = (low + high)//2
+            
+            if A[mid] == B:
+                return mid
+            
+            elif A[mid] > B:
+                high = mid - 1
+            else:
+                low = mid + 1
+            
+        return low
